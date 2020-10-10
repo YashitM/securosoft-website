f8106e7badf3c3bc759421feec4924494752d69c
YashitM <yashitmaheshwary@gmail.com>
2020-10-11 01:38:14 +0530

    Updated career page

---
+++ jobs.js
@@ -0,0 +1,46 @@
+var positions_json = [
+    {
+        "Full Stack Web Developer": {
+            "Header": `Urgently looking to hire Full Stack Web Developers for a live project with a Client based in UK.`,
+            "Requirements": `Should have around 1-2 years of experience in fundamental front end languages such as <b>HTML, CSS</b> and <b>JavaScript</b>.
+                Experience in Backend Frameworks/Languages like: <b>PHP/MERN Stack/Django/RoR/.NET</b> etc. preferred
+                Problem Solving Skills
+                Work from home. Around 2-4 hours daily.`,
+            "Location": "Remote",
+        }
+    },
+    {
+        "SOC Analyst": {
+            "Header": `Looking for talented and experienced SOC Analysts!`,
+            "Requirements": `Responsible for working in a 24/7 SOC environment.
+                Provide analysis and trending of security events, alarms and information from a large number of security and network devices.
+                Perform real-time monitoring, security incident handling, investigation, analysis, reporting and escalations of security events from multiple log sources through LogRhythm.
+                Provide Incident Response support when analysis confirms actionable incident.
+                Triage, Investigate, document, and report on information security events.
+                Integrate and share information with other analysts and other teams.
+                Experience with Enterprise tools like Carbon Black, Splunk, Fireeye Helix/IBM Qradar etc.`,
+            "Location": "Remote",
+        },
+    },
+    {
+        "Mobile App Developer (iOS/Android)": {
+            "Header": `Urgently looking to hire Mobile App Developers for a live project with a Startup based in UAE. `,
+            "Requirements": `Should have around 1-2 years of experience in Android/iOS App Development.
+                Creative mindset to design UI/UX and plan-innovate new features. 
+                Should support the entire application lifecycle (concept, design, test, release and support).
+                Work from home. Around 2-4 hours daily.`,
+            "Location": "Remote",
+        },
+    },
+    {
+        "React Native Developer": {
+            "Header": `Urgently looking to hire React Native Developers! `,
+            "Requirements": `Must have 1+ year experience developing and shipping user-facing features and applications using React Native.
+                Leverage native APIs for deeper integrations with iOS/Android platforms.
+                Working with React Native or other technologies as part of an Agile team developing mobile apps for iOS and Android
+                Preferred 2+ years’ experience in UI/UX development
+                Experience with popular React workflows (eg. Redux, Flux)`,
+            "Location": "Remote",
+        },
+    },
+]
